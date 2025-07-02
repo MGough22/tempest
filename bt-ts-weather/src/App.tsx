d8d6780576c2f962a3cf281f7f24d0e3130fc158
MGough 22 <gaivsvalerivs@gmail.com>
2025-07-02 11:57:45 +0100

    adds basic weather functionality for current location

--- bt-ts-weather/src/App.tsx
+++ bt-ts-weather/src/App.tsx
@@ -7,7 +7,20 @@ import Layout from "./components/ui/layout";
 import { ThemeProvider } from "./context/theme-provider";
 import WeatherDashboard from "./pages/weather-dashboard";
 import CityPage from "./pages/city";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 // import { Button } from "./components/ui/button";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      gcTime: 10 * 60 * 1000,
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -17,16 +30,19 @@ function App() {
     //   placeholder
     //   <Button>Test</Button>
     // </div>
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="dark">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<WeatherDashboard />} />
-            <Route path="/city/:cityName" element={<CityPage />} />
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
--- bt-ts-weather/src/pages/weather-dashboard.tsx
+++ bt-ts-weather/src/pages/weather-dashboard.tsx
@@ -1,7 +1,121 @@
+import { API_CONFIG } from "@/api/config";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import CurrentWeather from "@/components/ui/current-weather";
+import WeatherSkeleton from "@/components/ui/loading-skeleton";
+import { useGeoLocation } from "@/hooks/use-geolocation";
+import {
+  useForecastQuery,
+  useReverseGeocodeQuery,
+  useWeatherQuery,
+} from "@/hooks/use-weather";
+import { AlertTriangle, MapPin, RefreshCw } from "lucide-react";
 import React from "react";
 
 const WeatherDashboard = () => {
-  return <div>WeatherDashboard</div>;
+  const {
+    coordinates,
+    error: locationError,
+    getLocation,
+    isLoading: locationLoading,
+  } = useGeoLocation();
+
+  //   console.log("here", coordinates);
+  //   console.log("error", locationError);
+
+  const locationQuery = useReverseGeocodeQuery(coordinates);
+  const weatherQuery = useWeatherQuery(coordinates);
+  const forecastQuery = useForecastQuery(coordinates);
+
+  //   console.log("config ", API_CONFIG);
+  //   console.log("locationQuery should be here: ", locationQuery);
+
+  const handleRefresh = () => {
+    getLocation();
+    if (coordinates) {
+      locationQuery.refetch();
+      weatherQuery.refetch();
+      forecastQuery.refetch();
+    }
+  };
+
+  if (locationLoading) {
+    return <WeatherSkeleton />;
+  }
+
+  if (locationError) {
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  const locationName = locationQuery.data?.[0];
+
+  if (weatherQuery.error || forecastQuery.error) {
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Failed to fetch weather data. Please try again</p>
+          <Button onClick={handleRefresh} variant={"outline"} className="w-fit">
+            <RefreshCw className="mr-2 h-4 w-4" />
+            Retry
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (!weatherQuery.data || !forecastQuery.data) {
+    return <WeatherSkeleton />;
+  }
+
+  return (
+    <div className="space-y-4">
+      {/* Favourite cities */}
+      <div className="flex items-center justify-between">
+        {/* add horizontal padding later */}
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          onClick={handleRefresh}
+          disabled={weatherQuery.isFetching || forecastQuery.isFetching}
+        >
+          <RefreshCw
+            className={`h-4 w-4 ${
+              weatherQuery.isFetching ? "animate-spin" : ""
+            }`}
+          />
+        </Button>
+      </div>
+      {/* Current and hourly weather */}
+      <div className=" grid gap-6">
+        <div>
+          {/* TODO hourly */}
+          <CurrentWeather
+            data={weatherQuery.data}
+            locationName={locationName}
+          />
+        </div>
+        <div>
+          {/* TODO details
+            TODO forecast */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WeatherDashboard;
