11cbbf3aa8b094f8daea2026abf296cea87cbea1
MGough 22 <gaivsvalerivs@gmail.com>
2025-07-02 05:38:39 +0100

    adds provisional layout

--- bt-ts-weather/src/App.tsx
+++ bt-ts-weather/src/App.tsx
@@ -1,17 +1,25 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import { Button } from "./components/ui/button";
+import Layout from "./components/ui/layout";
+import { ThemeProvider } from "./context/theme-provider";
+// import { Button } from "./components/ui/button";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div>
-      placeholder
-      <Button>Test</Button>
-    </div>
+    // <div>
+    //   placeholder
+    //   <Button>Test</Button>
+    // </div>
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark">
+        <Layout>Hello</Layout>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
